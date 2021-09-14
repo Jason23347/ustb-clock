1b9e9cd8d936b7e9103f1f0c467f321b568e3dbf
Jason <jason23347@gmail.com>
2021-09-14 10:40:07 +0800

    opt: 优化draw_digit，还是不枚举了吧

--- src/tui/draw.c
+++ src/tui/draw.c
@@ -23,6 +23,7 @@ draw_line(offset_t offset, const char *prompt, const calc_t *value,
 }
 
 #ifdef CLOCK_DOT_WIDTH
+
 #if (CLOCK_DOT_WIDTH == 1)
 #define EMP " "
 #elif (CLOCK_DOT_WIDTH == 2)
@@ -30,8 +31,8 @@ draw_line(offset_t offset, const char *prompt, const calc_t *value,
 #else
 #define EMP "   "
 #endif
+
 #define DOT color(CLOCK_COLOR) EMP "\033[0m"
-#endif /* CLOCK_DOT_WIDTH */
 
 #if (CLOCK_CONDENSE == 0)
 #define POINT (digit & 0x4000) ? (DOT " ") : (EMP " ")
@@ -39,7 +40,9 @@ draw_line(offset_t offset, const char *prompt, const calc_t *value,
 #define POINT (digit & 0x4000) ? DOT : EMP
 #endif
 
-void
+#endif /* CLOCK_DOT_WIDTH */
+
+inline void
 __draw_digit(offset_t offset, int digit) {
     for (int i = 0; i < 5; i++) {
         gotopos(offset);
@@ -75,42 +78,23 @@ __draw_digit(offset_t offset, int digit) {
  */
 void
 draw_digit(offset_t offset, int digit) {
-#define DIGIT0     0x7B6F // 0
-#define DIGIT1     0x2492 // 1
-#define DIGIT2     0x73E7 // 2
-#define DIGIT3     0x73CF // 3
-#define DIGIT4     0x5BC9 // 4
-#define DIGIT5     0x79CF // 5
-#define DIGIT6     0x79EF // 6
-#define DIGIT7     0x7249 // 7
-#define DIGIT8     0x7BEF // 8
-#define DIGIT9     0x7BC9 // 9
-#define DIGIT_SEP  0x0410 // :
-#define digit(num) DIGIT##num
+    int map[10] = {
+        0x7B6F, // 0
+        0x2492, // 1
+        0x73E7, // 2
+        0x73CF, // 3
+        0x5BC9, // 4
+        0x79CF, // 5
+        0x79EF, // 6
+        0x7249, // 7
+        0x7BEF, // 8
+        0x7BC9, // 9
+    };
 
-    /* 枚举大法好 */
-    if (digit == 0) {
-        __draw_digit(offset, digit(0));
-    } else if (digit == 1) {
-        __draw_digit(offset, digit(1));
-    } else if (digit == 2) {
-        __draw_digit(offset, digit(2));
-    } else if (digit == 3) {
-        __draw_digit(offset, digit(3));
-    } else if (digit == 4) {
-        __draw_digit(offset, digit(4));
-    } else if (digit == 5) {
-        __draw_digit(offset, digit(5));
-    } else if (digit == 6) {
-        __draw_digit(offset, digit(6));
-    } else if (digit == 7) {
-        __draw_digit(offset, digit(7));
-    } else if (digit == 8) {
-        __draw_digit(offset, digit(8));
-    } else if (digit == 9) {
-        __draw_digit(offset, digit(9));
-    } else { // 冒号
-        __draw_digit(offset, DIGIT_SEP);
+    if (digit < 10 && digit >= 0) {
+        __draw_digit(offset, map[digit]);
+    } else { // :
+        __draw_digit(offset, 0x0410);
     }
 }
 
