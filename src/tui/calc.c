99a517f67dd8aa9224197e8897cccd3cf7ad8c83
Jason <jason23347@gmail.com>
2021-09-16 23:56:58 +0800

    opt: 重构浮点数计算

--- src/tui/calc.c
+++ src/tui/calc.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#define round(n) (unsigned long)(n + 0.5)
+
 inline int
 color_fee(unsigned fee) {
     if (fee >= 200000) {
@@ -15,15 +17,30 @@ color_fee(unsigned fee) {
     }
 }
 
+/* 手动添加小数点，保留两位小数 */
+const char *
+__calc_decimal(char *str, size_t maxlen, unsigned long number, size_t n) {
+    size_t len;
+    int tmp;
+
+    snprintf(str, maxlen, "%lu", number);
+    len = strlen(str);
+    tmp = len - n;
+    if (tmp < 1) {
+        strncpy(str + 1 - tmp, str, len);
+        memset(str, '0', 1 - tmp);
+        tmp = 3 - n;
+    }
+    strncpy(str + tmp + 1, str + tmp, 2);
+    str[tmp] = '.';
+    str[tmp + 3] = '\0';
+
+    return str;
+}
+
 calc_t *
 calc_fee(calc_t *calc, unsigned fee) {
-    snprintf(calc->str, sizeof(calc->str), "%d", fee);
-    /* 手动添加小数点 */
-    size_t len = strlen(calc->str);
-    strncpy(calc->str + len - 3, calc->str + len - 4, 2);
-    calc->str[len - 4] = '.';
-    calc->str[len - 1] = '\0';
-
+    __calc_decimal(calc->str, sizeof(calc->str), fee, 4);
     calc->color = color_fee(fee);
 
     return calc;
@@ -31,24 +48,34 @@ calc_fee(calc_t *calc, unsigned fee) {
 
 calc_t *
 calc_flow(calc_t *calc, unsigned long flow) {
-    if (flow / 1024 < 1) {
+    if (flow < 1000) {
         snprintf(calc->str, sizeof(calc->str), "%lu KB", flow);
-    } else if (flow / 1048576 < 1) {
-        snprintf(calc->str, sizeof(calc->str), "%.2f MB", ((float)flow / 1024));
-    } else {
-        float gbflow = (float)flow / 1048576;
-        /* 前 50G 免费，所以显示剩余免费额度 */
-        gbflow -= 50;
-        snprintf(calc->str, sizeof(calc->str), "%.2f GB", gbflow);
+        calc->color = NORMAL;
+        return calc;
     }
 
-    calc->color = NORMAL;
+    /* gbflow = 100 代表 1G */
+    unsigned long gbflow = round((float)flow * 100 / 1048576);
+    if (gbflow < 100) {
+        __calc_decimal(calc->str, sizeof(calc->str), flow, 2);
+        strncat(calc->str, " MB", sizeof(calc->str));
+        calc->color = NORMAL;
+    } else {
+        /* 前 50G 免费，所以显示剩余免费额度或已付费流量 */
+        gbflow -= 5000;
+
+        __calc_decimal(calc->str, sizeof(calc->str), gbflow, 2);
+        strncat(calc->str, " GB", sizeof(calc->str));
+        calc->color = NORMAL;
+    }
 
     return calc;
 }
 
 calc_t *
 calc_speed(calc_t *calc, unsigned long flow) {
+    if (flow < 8)
+        flow = 0;
     calc_flow(calc, flow);
     strncat(calc->str, "/s", sizeof(calc->str));
 
