83f5c95eda846f5f7583ea0b3b195857c3e6292d
Jason <jason23347@gmail.com>
2021-12-07 14:11:45 +0800

    chore: 优化digits.h，添加注释

--- src/tui/digits.h
+++ src/tui/digits.h
@@ -8,26 +8,21 @@
 #define CLOCK_DOTS_OFFSET (CLOCK_SPACE_WIDTH * 2 / 3)
 
 #if (CLOCK_CONDENSE == 0)
-
-#define CLOCK_DIGIT_WIDTH (3 * CLOCK_DOT_WIDTH + 2) // 三个点之间的俩空格
-
-/* 10 (clock) + 2 (date) + 4 (flow info) */
+/* 2 是三个点之间的俩空格 */
+#define CLOCK_DIGIT_WIDTH  (3 * CLOCK_DOT_WIDTH + 2)
 #define CLOCK_DIGIT_HEIGHT 10
-#define CLOCK_MIN_HEIGHT   16
 
-#else /* CLOCK_CONDENSE */
-
+#else
 #define CLOCK_DIGIT_WIDTH  (3 * CLOCK_DOT_WIDTH)
-
-/*  6 (clock) + 2 (date) + 4 (flow info) */
 #define CLOCK_DIGIT_HEIGHT 6
-#define CLOCK_MIN_HEIGHT   12
 
 #endif /* CLOCK_CONDENSE */
 
-/* Space between dots and digits is thinner, so minus 2 * offset */
+/* Space between dots and digits is thinner, so minus 2 offsets */
 #define CLOCK_MIN_WIDTH                                                        \
     (5 * CLOCK_DIGIT_WIDTH + 4 * CLOCK_SPACE_WIDTH - 2 * CLOCK_DOTS_OFFSET)
+/* (clock) + 2 (date) + 4 (flow info) */
+#define CLOCK_MIN_HEIGHT (CLOCK_DIGIT_HEIGHT + 6)
 
 typedef struct {
     time_t current;
@@ -39,6 +34,5 @@ int digits_init(digits_t *digits);
 int digits_update(digits_t *digits, struct timeval *new_time);
 void digits_setpos(int x, int y);
 void digits_redraw(digits_t *digits, struct timeval *new_time);
-void clock_draw_digit(int digit);
 
 #endif /* DIGITS_H */
